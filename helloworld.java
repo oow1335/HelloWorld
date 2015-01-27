bc936dd21d3e47b5d6c5f396dd21dcfecd52b44e
Oluwayemi Ogunbameru <yemio@d1stkfactory>
2015-01-27 02:19:51 +0000

    The heloworld git clone

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+/* This is the Helloworld program
+	From p.76 of the book
+		By Yemi Ogunbameru
+*/
+
+class HelloWorld {
+	Public tatic void main( String[] arg) {
+		System.out.print("Hello, world");
+	}
+}
+
+
